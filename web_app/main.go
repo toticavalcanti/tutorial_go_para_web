ade992df9d4f0cc7f001e0d55e43c8c2dcdbc8d9
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-01-26 21:02:58 -0300

    use gorilla mux to route

--- web_app/main.go
+++ web_app/main.go
@@ -6,12 +6,14 @@ import(
 	"github.com/gorilla/mux"
  )	
 
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8000", nil)
+}
+
  //request handle
  func handler(w http.ResponseWriter, r *http.Request){
  	fmt.Fprint(w, "Hello world!")
  }
-
-func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
-}
