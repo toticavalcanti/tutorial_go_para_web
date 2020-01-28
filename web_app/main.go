656d7636f5f28daead8b3291e43c0a35b0e9552b
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-01-28 11:11:27 -0300

    create a global var templates

--- web_app/main.go
+++ web_app/main.go
@@ -6,6 +6,8 @@ import(
 	"html/template"
  )	
 
+var templates *template.template
+
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloHandler).Methods("GET")
@@ -15,11 +17,6 @@ func main(){
 }
 
  //request hello handle
- func helloHandler(w http.ResponseWriter, r *http.Request){
+ func indexHandler(w http.ResponseWriter, r *http.Request){
  	fmt.Fprint(w, "Hello world!")
- }
-
- //request goodbye handle
- func goodbyeHandler(w http.ResponseWriter, r *http.Request){
- 	fmt.Fprint(w, "Goodbye world!")
  }
