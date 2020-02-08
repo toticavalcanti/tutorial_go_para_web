51a40be6652bee838258916ac86d2c9b5d6a5219
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-02-08 12:21:56 -0300

    change the example to use r.URL.Path

--- web_app/main.go
+++ web_app/main.go
@@ -1,17 +1,16 @@
 package main
 
-import(
-	"fmt"
-	"log"
-	"net/http"
- )	
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
 
- //request handle
- func handler(w http.ResponseWriter, r *http.Request){
- 	fmt.Fprint(w, "Hello world!")
- }
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, i like %s!", r.URL.Path[1:])
+}
 
-func main(){
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8000", nil))
 }
