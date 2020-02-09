359f3d2400e6e60bcefed6e12fdbc40307251b5d
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-02-09 11:58:38 -0300

    update main.go

--- web_app/main.go
+++ web_app/main.go
@@ -11,16 +11,22 @@ func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/hi", hiHandler).Methods("GET")
 	r.HandleFunc("/bye", byeHandler).Methods("GET")
+	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
- //request hello handle
+ //request index handle
+ func indexHandler(w http.ResponseWriter, r *http.Request){
+ 	fmt.Fprint(w, "This is the index page of your web aplication!")
+ }
+
+ //request hi everyone! handle
  func hiHandler(w http.ResponseWriter, r *http.Request){
  	fmt.Fprint(w, "Hi everyone!")
  }
 
- //request goodbye handle
+ //request bye bye! handle
  func byeHandler(w http.ResponseWriter, r *http.Request){
  	fmt.Fprint(w, "bye bye!")
  }
