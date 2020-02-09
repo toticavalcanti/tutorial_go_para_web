448475b1895b20b8579b5b7535c7ee3ff8c64d9e
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-02-09 13:26:02 -0300

    update main.go

--- web_app/main.go
+++ web_app/main.go
@@ -9,8 +9,8 @@ import(
 
 func main(){
 	r := mux.NewRouter()
-	r.HandleFunc("/hi", hiHandler).Methods("GET")
-	r.HandleFunc("/bye", byeHandler).Methods("GET")
+	r.HandleFunc("/contact", hiHandler).Methods("GET")
+	r.HandleFunc("/about", byeHandler).Methods("GET")
 	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8000", nil))
@@ -18,15 +18,15 @@ func main(){
 
  //request index page handle
  func indexHandler(w http.ResponseWriter, r *http.Request){
- 	fmt.Fprint(w, "This is the index page of your web aplication!")
+ 	fmt.Fprint(w, "This is the index page!")
  }
 
  //request hi everyone! page handle
- func hiHandler(w http.ResponseWriter, r *http.Request){
- 	fmt.Fprint(w, "Hi everyone!")
+ func contactHandler(w http.ResponseWriter, r *http.Request){
+ 	fmt.Fprint(w, "This is the contact page!")
  }
 
  //request bye bye! page handle
- func byeHandler(w http.ResponseWriter, r *http.Request){
- 	fmt.Fprint(w, "bye bye!")
+ func aboutHandler(w http.ResponseWriter, r *http.Request){
+ 	fmt.Fprint(w, "This is the about page!")
  }
