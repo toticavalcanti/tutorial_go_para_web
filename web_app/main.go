28363ab5b36b1a1b79b50ce4e7b05a50f0005108
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-10 11:06:15 -0300

    update project

--- web_app/main.go
+++ web_app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/go-redis/redis"
@@ -16,16 +17,24 @@ func main(){
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
+	r.HandleFunc("/contact", contactHandler).Methods("GET")
+	r.HandleFunc("/about", aboutHandler).Methods("GET")
 	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
-	http.ListenAndServe(":8000", nil)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
- //request hello handle
+ //request index page handle
  func indexHandler(w http.ResponseWriter, r *http.Request){
- 	comments, err := client.LRange("comments", 0, 10).Result()
- 	if err != nil{
- 		return
- 	}
- 	templates.ExecuteTemplate(w, "index.html", comments)
- }
+	templates.ExecuteTemplate(w, "index.html", "This is the index page!")
+}
+
+//request contact page handle
+func contactHandler(w http.ResponseWriter, r *http.Request){
+   templates.ExecuteTemplate(w, "contact.html", "This is the contact page!")
+}
+
+//request about page handle
+func aboutHandler(w http.ResponseWriter, r *http.Request){
+   templates.ExecuteTemplate(w, "about.html", "This is the about page!")
+}
