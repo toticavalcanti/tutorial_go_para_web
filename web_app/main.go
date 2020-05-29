86e1ad0623a4b46cae06d9c2421956c899733429
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-05-29 08:57:42 -0300

    fix main.go

--- web_app/main.go
+++ web_app/main.go
@@ -1,55 +1,59 @@
 package main
 
-import(
-	"net/http"
-	"github.com/gorilla/mux"
-	"github.com/go-redis/redis"
+import (
 	"html/template"
- )	
+	"net/http"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+)
+
 //globals variables
 var client *redis.Client
 var templates *template.Template
 
-func main(){
+func main() {
 	client = redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/contact", contactHandler).Methods("GET") 
+	r.HandleFunc("/contact", contactHandler).Methods("GET")
 	r.HandleFunc("/about", aboutHandler).Methods("GET")
 	r.HandleFunc("/", indexGetHandler).Methods("GET")
 	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
 	http.ListenAndServe(":8001", nil)
 }
 
- //request hello handle
- func indexGetHandler(w http.ResponseWriter, r *http.Request){
- 	comments, err := client.LRange("comments", 0, 10).Result()
- 	if err != nil{
- 		return
- 	}
- 	templates.ExecuteTemplate(w, "index.html", comments)
- }
+//request index handle
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
 
 //request index page POST handle
-  func indexPostHandler(w http.ResponseWriter, r *http.Request){
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	//get the comment in html tag comment
 	comment := r.PostForm.Get("comment")
 	//push the comment to the comments list
 	client.LPush("comments", comment)
-	 // redirect to / when the submit form
- 	http.Redirect(w, r, "/", 302)
- }
+	// redirect to / when the submit form
+	http.Redirect(w, r, "/", 302)
+}
 
- //request contact page handle
-func contactHandler(w http.ResponseWriter, r *http.Request){
+//request contact page handle
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "contact.html", "This is the contact page!")
- }
- 
- //request about page handle
- func aboutHandler(w http.ResponseWriter, r *http.Request){
+}
+
+//request about page handle
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "about.html", "This is the about page!")
- }
+}
