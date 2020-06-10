1b13b784da24937c7a274b4a09d417c02fa16465
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-10 20:28:48 -0300

    put about and contact handler

--- web_app/main.go
+++ web_app/main.go
@@ -1,23 +1,27 @@
 package main
 
-import(
+import (
+	"html/template"
 	"net/http"
+
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"github.com/go-redis/redis"
-	"html/template"
- )	
+)
+
 //globals variables
 var client *redis.Client
 var store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
 var templates *template.Template
 
-func main(){
+func main() {
 	client = redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
+	r.HandleFunc("/contact", contactHandler).Methods("GET")
+	r.HandleFunc("/about", aboutHandler).Methods("GET")
 	r.HandleFunc("/", indexGetHandler).Methods("GET")
 	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
@@ -29,27 +33,27 @@ func main(){
 	http.ListenAndServe(":8000", nil)
 }
 
- //request hello handle
- func indexGetHandler(w http.ResponseWriter, r *http.Request){
- 	comments, err := client.LRange("comments", 0, 10).Result()
- 	if err != nil{
- 		return
- 	}
- 	templates.ExecuteTemplate(w, "index.html", comments)
- }
+//request hello handle
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
 
-  func indexPostHandler(w http.ResponseWriter, r *http.Request){
- 	r.ParseForm()
- 	comment := r.PostForm.Get("comment")
- 	client.LPush("comments", comment)
- 	http.Redirect(w, r, "/", 302)
- }
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, "/", 302)
+}
 
-func loginGetHandler(w http.ResponseWriter, r *http.Request){
+func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "login.html", nil)
 }
 
-func loginPostHandler(w http.ResponseWriter, r *http.Request){
+func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.PostForm.Get("username")
 	session, _ := store.Get(r, "session")
@@ -57,15 +61,25 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request){
 	session.Save(r, w)
 }
 
- func testGetHandler(w http.ResponseWriter, r *http.Request){
- 	session, _ := store.Get(r, "session")
- 	untyped, ok := session.Values["username"]
- 	if !ok{
- 		return
- 	}
- 	username, ok := untyped.(string)
- 	if !ok{
- 		return
- 	}
- 	w.Write([]byte(username))
- }
+func testGetHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	untyped, ok := session.Values["username"]
+	if !ok {
+		return
+	}
+	username, ok := untyped.(string)
+	if !ok {
+		return
+	}
+	w.Write([]byte(username))
+}
+
+//request contact page handle
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "contact.html", "This is the contact page!")
+}
+
+//request about page handle
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "about.html", "This is the about page!")
+}
